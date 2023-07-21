23d024eee9df3887c20eac451e38662eda08e586
ChuongNguyen1704 <74581163+ChuongNguyen1704@users.noreply.github.com>
2023-07-21 14:54:50 +0700

    Update EDA.sql

--- E-commerce/EDA.sql
+++ E-commerce/EDA.sql
@@ -105,7 +105,7 @@ ORDER BY payment_installments DESC;
     JOIN dbo.order_reviews ON order_reviews.order_id = oi.order_id
 
 /*...................... Question 6: The number of reviews by review category. ...................... */
--- View + CTEs
+-- View + CTEs + CASE WHEN + Independenly Nested Query
 CREATE VIEW review_score_percentage AS
 WITH total_review_score AS (
     SELECT
@@ -132,8 +132,26 @@ FROM total_review_score
 GROUP BY review_status;
 
 
+/*...................... Question 7: The total number of the payment value of each payment type.   ...................... */
+
+-- CTEs + CROSS JOIN
+CREATE VIEW payment_type AS
+WITH TotalSum AS (
+    SELECT ROUND(SUM(payment_value), 2) AS total_sum
+    FROM dbo.order_payments
+)
+SELECT
+    op.payment_type,
+	ROUND(SUM(op.payment_value), 2) AS total_value,
+    CONCAT(FORMAT(SUM(op.payment_value) * 100.0 / ts.total_sum, 'N2'), '%') AS percentage
+FROM dbo.order_payments AS op
+CROSS JOIN TotalSum AS ts 
+GROUP BY op.payment_type, ts.total_sum
+HAVING ROUND(SUM(op.payment_value), 2) > 0;
+
+
 /*......................		customers & order_payments & order		...................... */
-/*...................... Question 7: GMV Distribution by States. ...................... */
+/*...................... Question 8: GMV Distribution by States. ...................... */
 
 -- Views + Window Function
 CREATE VIEW dbo.state_percentage AS
@@ -148,11 +166,82 @@ FROM (
     FROM dbo.customers AS c
     JOIN dbo.[order] AS o ON o.customer_id = c.customer_id
     JOIN dbo.order_payments AS op ON op.order_id = o.order_id
-    GROUP BY c.customer_state
-) AS subquery
+    GROUP BY c.customer_state) AS subquery
 
 SELECT * FROM dbo.state_percentage ORDER BY revenue DESC;
 
-/*...................... Question 7: Find the OrderID whose maximum Quantity among all product of that OrderID 
-															is greater than average quantity of all OrderID. ...................... */
 
+/*......................		Calendar & order		...................... */
+/*...................... Question 9: The number of orders by days of week. ...................... */
+SELECT 
+    c.DayOfWeek,
+    COUNT(o.[order_id]) AS NumberOfOrders,
+    CONCAT(FORMAT(COUNT(o.[order_id]) * 100.0 / SUM(COUNT(o.[order_id])) OVER (), 'N2'), '%') AS Percentage
+FROM 
+    dbo.Calendar c
+LEFT JOIN 
+    dbo.[order] AS o
+    ON DATEPART(weekday, o.[order_estimated_delivery_date]) = DATEPART(weekday, c.CalendarDate)
+GROUP BY
+    c.DayOfWeek
+HAVING 
+    COUNT(o.[order_id]) > 2
+ORDER BY 
+  Percentage DESC;
+
+
+/*......................		Calendar & order & order_payments		...................... */
+/*...................... Question 10: The revenue of all months by each year. ...................... */
+
+WITH TotalSum AS (
+    SELECT 
+        DATEPART(YEAR, o.order_estimated_delivery_date) AS [Year],
+        ROUND(SUM(op.payment_value), 2) AS total_sum
+    FROM dbo.[order] AS o
+    JOIN dbo.[order_payments] AS op ON o.order_id = op.order_id
+    GROUP BY DATEPART(YEAR, o.order_estimated_delivery_date)
+)
+SELECT 
+    c.Year,
+    c.Month,
+    ROUND(SUM(op.payment_value), 2) AS total_revenue,
+    CONCAT(FORMAT(ROUND(SUM(op.payment_value) * 100.0 / ts.total_sum, 2), 'N2'), '%') AS percentage
+FROM dbo.[order] AS o
+JOIN dbo.[order_payments] AS op ON o.order_id = op.order_id
+JOIN dbo.[Calendar] AS c ON c.CalendarDate = o.order_estimated_delivery_date
+JOIN TotalSum as ts ON DATEPART(YEAR, c.CalendarDate) = ts.[Year]
+GROUP BY c.Year, c.Month, ts.total_sum
+HAVING ROUND(SUM(op.payment_value) * 100.0 / ts.total_sum, 2) > 0
+ORDER BY c.Year, c.Month DESC;
+
+
+
+/*......................		Calendar & payments_value		...................... */
+/*.............. Question 11: All months by each year have more revenue than the average of all months by each year. .............. */
+
+WITH MonthlyRevenue AS (
+    SELECT 
+        c.Year,
+        c.Month,
+        ROUND(SUM(op.payment_value), 2) AS total_revenue
+    FROM dbo.[order] AS o
+    JOIN dbo.[order_payments] AS op ON o.order_id = op.order_id
+    JOIN dbo.[Calendar] AS c ON c.CalendarDate = o.order_estimated_delivery_date
+    GROUP BY c.Year, c.Month
+),
+AverageRevenueByYear AS (
+    SELECT
+        Year,
+        ROUND(AVG(total_revenue), 2) AS avg_revenue
+    FROM MonthlyRevenue
+    GROUP BY Year
+)
+SELECT 
+    m.Year,
+    m.Month,
+    m.total_revenue,
+    a.avg_revenue AS average_revenue
+FROM MonthlyRevenue m
+JOIN AverageRevenueByYear a ON m.Year = a.Year
+WHERE m.total_revenue > a.avg_revenue
+ORDER BY m.Year, m.Month DESC;
